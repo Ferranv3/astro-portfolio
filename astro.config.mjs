be2b9335fb9c9292ab3fbb5d6b0b1ae98614b6e2
Ferranv3 <ferranhpv3@gmail.com>
2023-05-12 12:55:15 +0200

    Vercel changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+    cacheDir: "./.cache/image",
+    logLevel: 'debug'
+  })],
+  output: "server",
+  adapter: vercel()
 });
