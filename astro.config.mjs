1ad9633f3183fe2f9d95d234a1a81964abda0a98
Ferranv3 <ferranhpv3@gmail.com>
2023-05-12 12:57:31 +0200

    Fix imports astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
