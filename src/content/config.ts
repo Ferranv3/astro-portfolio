91dff176ed3b321470a0df8231eca2535ddda569
FerranHernandez <ferranhpv3@gmail.com>
2024-04-14 15:11:25 +0200

    Optimize images and add npmignore

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,26 +9,10 @@ const blogSchema = z.object({
     badge: z.string().optional(),
 });
 
-const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing:  z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-});
-
 export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
+    'blog': blogCollection
 }
