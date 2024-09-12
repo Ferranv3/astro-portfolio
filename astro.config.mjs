9b8f6e4e6e562b23ef9c3ed59b0eb8d3bc6e0fd6
FerranHernandez <ferranhpv3@gmail.com>
2024-09-12 15:27:43 +0200

    Update Astro 4 and fix image size

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'https://ferrranhpv3.vercel.app/',
-  integrations: [mdx(), sitemap(), tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-    cacheDir: "./.cache/image",
-    logLevel: 'debug'
-  }), react()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "server",
   adapter: vercel()
 });
